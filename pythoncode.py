735c2c6ab5a39bca4a0247d1e1160900a6e2c17e
Einar <einamundsen@gmail.com>
2023-10-30 05:04:54 +0100

    Added MSE and Rsquared

--- pythoncode.py
+++ pythoncode.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # Load the CSV data from the path using Pandas
 df = pd.read_csv('TSLA.csv')
@@ -14,7 +14,7 @@ df['Date'] = pd.to_datetime(df['Date'])
 # Output is price => Y = Close Price
 # Linear regression works with numerical features, so we need to convert DateTime to timestamps
 
-X = np.arange(1, len(df) +1).reshape(-1, 1)
+X = np.arange(1, len(df) + 1).reshape(-1, 1)
 Y = df['Close'].values.reshape(-1, 1)
 
 # Create a linear regression model
@@ -29,6 +29,12 @@ Y_pred = linear_regressor.predict(X)
 # Calculate Mean Absolute Error (MAE)
 mae = mean_absolute_error(Y, Y_pred)
 
+# Calculate Mean Squared Error (MSE)
+mse = mean_squared_error(Y, Y_pred)
+
+# Calculate R-squared (R²) value
+r2 = r2_score(Y, Y_pred)
+
 # Create the scatterplot
 plt.scatter(X, Y)
 plt.plot(X, Y_pred, color='red')
@@ -38,5 +44,7 @@ plt.ylabel('Close Price')
 # Show the plot
 plt.show()
 
-# Print the MAE value
+# Print the MAE, MSE, and R² values
 print(f"Mean Absolute Error (MAE): {mae:.2f}")
+print(f"Mean Squared Error (MSE): {mse:.2f}")
+print(f"R-squared (R²) Value: {r2:.2f}")
